2aba56e81d1982c8069fc2d1956d63f13bd76bba
DongHeon Lee <niche3600@gmail.com>
2021-06-14 13:00:31 +0900

    文言変更

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/', (req, res) => res.send('Express + TypeScript Server is work!'));
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
